d559bd5ea0594834442f35269884bd0d68f6e67c
PSSubramanyaBhat <subramanya@robosoftin.com>
2020-09-28 03:42:05 +0530

    Added code to chapter 4 and 5

--- 24th Sept/chap4_ex4.js
+++ 24th Sept/chap4_ex4.js
@@ -1,29 +1,30 @@
 "use strict";
 
-
-// console.log(Object.keys(obj).sort());
-
 function deepEqual(obj1, obj2) {
-    /*let a = [];
-    a.push(obj1);
-    a.push(obj2);
-	
-    if (JSON.stringify(a[0]) === JSON.stringify(a[1])) {
-    	return true;
-    } else {
-    	return false;
-    }*/
-	
-    if (JSON.stringify(obj1) === JSON.stringify(obj2)) {
+    if (obj1 === obj2) {
         return true;
-    } else {
+    } else if (obj1 === null || obj2 === null || typeof (obj1) !== "object" || typeof (obj2) !== "object") {
         return false;
+    } else if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+        return false;
+    } else {
+        for (let keyValue of Object.keys(obj1)) {
+            if (!deepEqual(obj1[keyValue], obj2[keyValue])) {
+                return false;
+            }
+        }
     }
+    return true;
 }
 
-let obj = {here: {is: "an"}, object: 2};
+let obj = { here: { is: "an" }, object: 2 };
+
+let a = null;
+
 console.log(deepEqual(obj, obj));
 // → true
+console.log(deepEqual(a, obj));
+// → false
 console.log(deepEqual(obj, {here: 1, object: 2}));
 // → false
 console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
